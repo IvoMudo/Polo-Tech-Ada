2b31371966ca32b84f3437f3115e9c459f01b914
Ivo Augusto <ivomudo@gmail.com>
2023-01-20 21:23:25 -0300

    atividade do dia 20/01/2023

---
+++ PROGRAMACAO ORIENTADA A OBJETOS I/src/atividade/ContaPj.java
@@ -0,0 +1,12 @@
+package atividade;
+
+public class ContaPj extends Conta{
+//    double saldo = 0;
+
+    public ContaPj(String id, Cliente donoDaConta) {
+        super(id, donoDaConta);
+    }
+    void doSomething(){
+//        this.saldo = 51;
+    }
+}
